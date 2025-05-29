98559bcbcdd55bea3cde13d3ac702c387820fe62
ahmed firoum <ahmedfiroumk@gmail.com>
2025-05-29 10:18:46 +0200

    04/JS-Monorepo-rounting advanced done

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,43 @@
+// Some data to make the trick
+
+import type { RequestHandler } from "express";
+
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+// Declare the actions
+const browse: RequestHandler = (req, res) => {
+  const query = (req.query.q as string).toLowerCase();
+  if (req.query.q != null) {
+    const filteredCategory = categories.filter((category) =>
+      category.name.toLowerCase().includes(query),
+    );
+    res.json(filteredCategory);
+  } else {
+    res.json(categories);
+  }
+};
+
+/* Here you code */
+const read: RequestHandler = (req, res) => {
+  const parseId = Number.parseInt(req.params.id);
+  const category = categories.find((p) => p.id === parseId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+// Export them to import them somewhere else
+
+/* Here you export */
+export default { browse, read };
--- server/src/router.ts
+++ server/src/router.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
+import categoryActions from "./modules/category/categoryActions";
 import itemActions from "./modules/item/itemActions";
 import programActions from "./modules/program/programActions";
 import sayActions from "./modules/say/sayActions";
@@ -19,4 +20,9 @@ router.post("/api/items", itemActions.add);
 
 router.get("/", sayActions.sayHello);
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+// Category
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 export default router;
