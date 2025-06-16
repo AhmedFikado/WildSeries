b5662cfd0edb66a1515c120b0233e1b84aca491d
ahmed firoum <ahmedfiroumk@gmail.com>
2025-06-16 09:48:45 +0200

    JS-Monorepo-validation done

---
+++ client/src/pages/CategoryDetails.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router";
+import CategoryDeleteForm from "../components/CategoryDeleteForm";
+import type { Program } from "../types/Program";
+
+type Category = {
+  id: number;
+  name: string;
+  programs: Program[];
+};
+
+function CategoryDetails() {
+  const { id } = useParams();
+  const [category, setCategory] = useState(null as null | Category);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/categories/${id}`)
+      .then((response) => response.json())
+      .then((data: Category) => {
+        setCategory(data);
+      });
+  }, [id]);
+
+  return (
+    category && (
+      <>
+        <hgroup className="details-hgroup">
+          <h1>{category.name}</h1>
+          <Link to={`/categories/${category.id}/edit`}>Modifier</Link>
+          <CategoryDeleteForm id={category.id}>Supprimer</CategoryDeleteForm>
+        </hgroup>
+
+        <ul>
+          {category.programs.map((program) => (
+            <li key={program.id}>
+              <Link to={`/programs/${program.id}`}>
+                <p>{program.title}</p>
+                <img src={program.poster} alt={`poster of ${program.title}`} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+  );
+}
+
+export default CategoryDetails;
--- server/src/router.ts
+++ server/src/router.ts
@@ -22,10 +22,17 @@ router.get("/", sayActions.sayHello);
 router.get("/api/programs", programActions.browse);
 router.get("/api/programs/:id", programActions.read);
 router.put("/api/programs/:id", programActions.edit);
-router.post("/api/programs", programActions.add);
+router.post("/api/programs", programActions.validate, programActions.add);
 router.delete("/api/programs/:id", programActions.destroy);
 
 // Category
 router.get("/api/categories", categoryActions.browse);
 router.get("/api/categories/:id", categoryActions.read);
+router.put(
+  "/api/categories/:id",
+  categoryActions.validate,
+  categoryActions.edit,
+);
+router.post("/api/categories", categoryActions.validate, categoryActions.add);
+router.delete("/api/categories/:id", categoryActions.destroy);
 export default router;
